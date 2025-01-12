720641a5014024e3bbe40edd16e582faa2f10261
Ali Raza <137720587+malirazaansari@users.noreply.github.com>
2025-01-12 20:44:22 +0500

    dashboard file done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
-// import Dashboard from "./components/Dashboard";
+import Dashboard from "./components/Dashboard";
 
 const App = () => {
   return (
@@ -15,10 +15,10 @@ const App = () => {
           path="/signup"
           element={<SignUp />}
         />
-        {/* <Route
+        <Route
           path="/dashboard"
           element={<Dashboard />}
-        /> */}
+        />
       </Routes>
     </Router>
   );
