258b604eefb1fe24b779bf7e388ee39e1c3f07fd
Ali Raza <137720587+malirazaansari@users.noreply.github.com>
2025-01-23 10:29:43 +0500

    Removed .env and sensitive files from tracking

--- frontend/src/store/firebase.js
+++ frontend/src/store/firebase.js
@@ -1,10 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAgofPmjAfMJ7oITF-6Q6cYLKj8nz9v8ec",
   authDomain: "olchat-auth.firebaseapp.com",
@@ -14,7 +9,6 @@ const firebaseConfig = {
   appId: "1:232862782311:web:279651e6ef4149aecd95c2",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
