d16efada7ee009ab06be775a0142bcd3b2a44c38
Ali Raza <137720587+malirazaansari@users.noreply.github.com>
2025-01-28 12:15:23 +0500

    from youtube vedio made some changes from which i see this

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,9 +8,13 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
 app.use((err, req, res, next) => {
   if (err.type === "entity.too.large") {
     return res.status(413).json({
@@ -21,10 +25,17 @@ app.use((err, req, res, next) => {
 });
 app.use(express.json());
 app.use(bodyParser.json({ limit: "10mb" }));
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("server listening on PORT:" + PORT);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_MAIN_URL
-    ? `${import.meta.env.VITE_MAIN_URL}/api`
-    : "http://localhost:5001/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "/api",
   // baseURL: `${import.meta.env.VITE_MAIN_URL}/api`,
   withCredentials: true, // To send cookies with requests
 });
