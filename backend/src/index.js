6603a5fb2338ab65b57e74e00ffe5329afb2f13e
Ali Raza <137720587+malirazaansari@users.noreply.github.com>
2025-01-29 11:21:49 +0500

    env change again for production as yt vedio does

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,8 @@ app.use((err, req, res, next) => {
 });
 app.use(express.json());
 app.use(bodyParser.json({ limit: "10mb" }));
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL:
-  //   import.meta.env.MODE === "development"
-  //     ? "http://localhost:5001/api"
-  //     : "/api",
-  baseURL: `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api`,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "/api",
+  // baseURL: `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api`,
   withCredentials: true,
 });
