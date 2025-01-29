b834d3348dbe2e725a6d4a3edbcb445749eda56f
Ali Raza <137720587+malirazaansari@users.noreply.github.com>
2025-01-29 10:22:05 +0500

    firebase files removed and env sample added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,7 +2,6 @@ import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import cloudinary from "../lib/cloudinary.js";
 import bcrypt from "bcrypt";
-import admin from "../lib/firebaseAdmin.js";
 
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -2,7 +2,6 @@ import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
   {
-    firebaseId: { type: String, unique: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: {
